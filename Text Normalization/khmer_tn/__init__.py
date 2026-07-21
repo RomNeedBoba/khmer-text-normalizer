49b718fa81a7b645dda1545c89b7674f5ea2dcfe
Rom Jr. <157264890+RomNeedBoba@users.noreply.github.com>
2026-07-21 10:13:07 +0700

    Delete Text Normalization directory

--- Text Normalization/khmer_tn/__init__.py
+++
@@ -1,5 +0,0 @@
-from .normalizer import KhmerNormalizer, DEFAULT_CATEGORIES
-from .clean import Cleaner
-from .segment import Segmenter
-
-__all__ = ["KhmerNormalizer", "DEFAULT_CATEGORIES", "Cleaner", "Segmenter"]
--- Text Normalization/khmer_tn/src/khmer_tn/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from khmer-tn!")
-
-
-if __name__ == "__main__":
-    main()
